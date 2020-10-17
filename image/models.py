8a1f2525493f1d5d368483ae56693c7e9b7d2083
richardkefa <richardkefa@gmail.com>
2020-10-18 00:17:21 +0300

    add Image class model

---
+++ image/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import datetime as dt
+
+class Image(models.Model):
+  image = models.ImageField()
+  image_name = models.CharField(max_length=150)
+  image_caption = models.TextField()
+  likes = models.IntegerField()
+  comments = models.TextField()
+  post_date = models.DateTimeField(auto_now_add=True)
+  profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.image_name
+  
